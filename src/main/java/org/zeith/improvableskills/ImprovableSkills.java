4c8794b59a14d1e3b562d46264cae2aa2a596dba
Zeith <me@zeith.org>
2024-01-21 02:50:48 +0200

    Merge poop

--- src/main/java/org/zeith/improvableskills/ImprovableSkills.java
+++ src/main/java/org/zeith/improvableskills/ImprovableSkills.java
@@ -1,37 +1,27 @@
 package org.zeith.improvableskills;
 
-import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.storage.loot.LootPool;
-import net.minecraft.world.level.storage.loot.entries.EmptyLootItem;
-import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.*;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
-import net.minecraftforge.common.*;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.*;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.*;
 import org.zeith.api.registry.RegistryMapping;
-import org.zeith.hammerlib.HammerLib;
-import org.zeith.hammerlib.core.RecipeHelper;
 import org.zeith.hammerlib.core.adapter.LanguageAdapter;
 import org.zeith.hammerlib.event.fml.FMLFingerprintCheckEvent;
-import org.zeith.hammerlib.event.recipe.RegisterRecipesEvent;
 import org.zeith.hammerlib.util.CommonMessages;
-import org.zeith.improvableskills.api.RecipeParchmentFragment;
 import org.zeith.improvableskills.api.loot.RandomBoolean;
 import org.zeith.improvableskills.api.registry.*;
 import org.zeith.improvableskills.cfg.ConfigsIS;
 import org.zeith.improvableskills.command.CommandImprovableSkills;
-import org.zeith.improvableskills.custom.items.ItemAbilityScroll;
 import org.zeith.improvableskills.init.*;
 import org.zeith.improvableskills.proxy.*;
 
@@ -80,8 +70,6 @@ public class ImprovableSkills
 		
 		mcfBus.addListener(this::registerCommands);
 		mcfBus.addListener(this::addLoot);
-		
-		HammerLib.EVENT_BUS.addListener(this::addRecipes);
 	}
 	
 	private void registerCommands(RegisterCommandsEvent e)
@@ -134,8 +122,14 @@ public class ImprovableSkills
 			}
 		}
 		
-		if(e.getName().toString().toLowerCase().contains("chests/"))
+		var ids = e.getName().toString();
+		if(ids.contains("chests/") && ConfigsIS.parchmentGeneration)
 		{
+			if(ConfigsIS.blockedParchmentChests.contains(ids))
+			{
+				ImprovableSkills.LOG.debug("SKIPPING parchment injection for LootTable '" + ids + "'!");
+				return;
+			}
 			RandomBoolean bool = new RandomBoolean();
 			bool.n = 5;
 			
@@ -146,7 +140,7 @@ public class ImprovableSkills
 				var table = e.getTable();
 				table.addPool(LootPool.lootPool()
 						.setRolls(ConstantValue.exactly(1F))
-						.add(EmptyLootItem.emptyItem().setWeight(4))
+						.add(EmptyLootItem.emptyItem().setWeight(ConfigsIS.parchmentRarity))
 						.add(LootItem.lootTableItem(ItemsIS.PARCHMENT_FRAGMENT)
 								.apply(SetItemCountFunction.setCount(ConstantValue.exactly(1F)))
 								.setWeight(1)
@@ -160,74 +154,6 @@ public class ImprovableSkills
 				err.printStackTrace();
 			}
 		}
-	}
-	
-	private void addRecipes(RegisterRecipesEvent e)
-	{
-		e.shaped()
-				.shape("lbl", "pgp", "lbl")
-				.map('l', Tags.Items.LEATHER)
-				.map('b', Items.BOOK)
-				.map('p', Items.PAPER)
-				.map('g', Tags.Items.INGOTS_GOLD)
-				.result(ItemsIS.SKILLS_BOOK)
-				.register();
-		
-		e.shapeless()
-				.add(ItemsIS.PARCHMENT_FRAGMENT)
-				.result(new ItemStack(Items.PAPER, 7))
-				.register();
-		
-		e.add(new RecipeParchmentFragment(AbilitiesIS.ANVIL.getRegistryName(),
-				ItemAbilityScroll.of(AbilitiesIS.ANVIL),
-				NonNullList.of(
-						Ingredient.EMPTY,
-						RecipeHelper.fromTag(Tags.Items.ENDER_PEARLS),
-						RecipeHelper.fromComponent(Items.ANVIL),
-						RecipeHelper.fromTag(Tags.Items.GEMS_EMERALD)
-				)
-		));
-		
-		e.add(new RecipeParchmentFragment(AbilitiesIS.CRAFTER.getRegistryName(),
-				ItemAbilityScroll.of(AbilitiesIS.CRAFTER),
-				NonNullList.of(
-						Ingredient.EMPTY,
-						RecipeHelper.fromTag(Tags.Items.ENDER_PEARLS),
-						RecipeHelper.fromComponent(Items.CRAFTING_TABLE),
-						RecipeHelper.fromTag(Tags.Items.INGOTS_IRON)
-				)
-		));
-		
-		e.add(new RecipeParchmentFragment(AbilitiesIS.ENCHANTING.getRegistryName(),
-				ItemAbilityScroll.of(AbilitiesIS.ENCHANTING),
-				NonNullList.of(
-						Ingredient.EMPTY,
-						RecipeHelper.fromTag(Tags.Items.ENDER_PEARLS),
-						RecipeHelper.fromComponent(Items.ENCHANTING_TABLE),
-						RecipeHelper.fromComponent(Items.BOOKSHELF)
-				)
-		));
-		
-		e.add(new RecipeParchmentFragment(AbilitiesIS.MAGNETISM.getRegistryName(),
-				ItemAbilityScroll.of(AbilitiesIS.MAGNETISM),
-				NonNullList.of(
-						Ingredient.EMPTY,
-						RecipeHelper.fromTag(Tags.Items.ENDER_PEARLS),
-						RecipeHelper.fromComponent(Items.ENDER_EYE),
-						RecipeHelper.fromComponent(Items.IRON_INGOT),
-						RecipeHelper.fromComponent(Items.CHAIN)
-				)
-		));
-		
-		e.add(new RecipeParchmentFragment(AbilitiesIS.AUTO_XP_BANK.getRegistryName(),
-				ItemAbilityScroll.of(AbilitiesIS.AUTO_XP_BANK),
-				NonNullList.of(
-						Ingredient.EMPTY,
-						RecipeHelper.fromTag(Tags.Items.ENDER_PEARLS),
-						RecipeHelper.fromComponent(Items.EXPERIENCE_BOTTLE),
-						RecipeHelper.fromComponent(Items.REDSTONE)
-				)
-		));
 	}
 	
 	public static IForgeRegistry<PlayerSkillBase> SKILLS()
